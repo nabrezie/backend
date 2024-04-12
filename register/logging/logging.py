7ea5755969849d34dee5357cd25defdfee5b28ef
Filip Žitný <filipzitny@gmail.com>
2024-04-12 14:23:32 +0200

    bug fix

--- register/logging/logging.py
+++ register/logging/logging.py
@@ -1,18 +1,10 @@
 """Module to log changes in the register and database."""
-import logging
-
-MODULE_LOGGER = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
 
 
-class RegisterLogger(logging.LoggerAdapter):
-    """Logger adapter to add module name to log messages, 
+class RegisterLogger:
+    """Logger adapter to add module name to log messages,
     for changes in registr and databse.
     """
-
-    def __init__(self, logger, extra):
-        """Initialize the logger adapter."""
-        super(RegisterLogger, self).__init__(logger, extra)
 
     def process(self, msg, kwargs):
         """Add module name to log messages."""
@@ -20,6 +12,3 @@ class RegisterLogger(logging.LoggerAdapter):
 
     def export_log(self, filename):
         """Export log to a file."""
-        with open(filename, 'w') as file:
-            for record in self.logger.handlers[0].buffer:
-                file.write(f'{record}\n')
